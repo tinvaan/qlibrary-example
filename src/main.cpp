6b576104fd1d807befb8f2e941c05d55f4a06cb5
R. Harish Navnit <rharish@spectralinsights.com>
2017-12-20 14:13:11 +0530

    Load symbols based on user input.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <iostream>
 
 // Qt includes
@@ -14,15 +15,36 @@ typedef double (*QuotientFunction)(double, double);
 typedef double (*DifferenceFunction)(double, double);
 
 
-double loadFromHeader();
-double loadFromLibrary();
+double loadFromHeader(int );
+double loadFromLibrary(int );
 
 int main(int argc, char *argv[])
 {
+	int operation = -1;
+	std::string choice;
+
+	std::cout << "Operation ('add', 'sub', 'div', 'avg') : ";
+	std::cin >> choice;
+
+	if (choice == "add" || choice == "Add")
+		operation = 1;
+	else if (choice == "sub" || choice == "Sub")
+		operation = 2;
+	else if (choice == "mul" || choice == "Mul")
+		operation = 3;
+	else if (choice == "div" || choice == "Div")
+		operation = 4;
+	else if (choice == "avg" || choice == "Avg")
+		operation = 5;
+	else {
+		std::cerr << "Invalid operation " << std::endl;
+		return -1;
+	}
+
 #if LIBRARY
-	double result = loadFromLibrary();
+	double result = loadFromLibrary(operation);
 #else
-	double result = loadFromHeader();
+	double result = loadFromHeader(operation);
 #endif
 
 	std::cout << "Result = " << result << std::endl;
@@ -30,26 +52,95 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-double loadFromHeader()
-{
-	Compute c;
-	return c.sum(4.4, 10);
-}
-
-double loadFromLibrary()
+double loadFromHeader(int op)
 {
 	double res;
+	Compute c;
+
+	switch(op) {
+		case 1:
+			res = c.sum(4.4, 10);
+			break;
+		case 2:
+			res = c.difference(4.4, 10);
+			break;
+		case 3:
+			res = c.product(4.4, 10);
+			break;
+		case 4:
+			res = c.quotient(4.4, 10);
+			break;
+		case 5:
+			res = c.average(4.4, 10);
+			break;
+		default: break;
+	}
+
+	return res;
+}
+
+double loadFromLibrary(int op)
+{
+	double res = 0.0;
 	bool error = false;
 	QLibrary computeLib("compute");
 
 	if (computeLib.load()) { 
 		if (computeLib.isLoaded()) {
-			SumFunction sum = (SumFunction)QLibrary::resolve("compute", "sum");
 
-			if (sum)
-				res = sum(4.4, 10);
-			else
-				error = true;
+			switch(op) {
+				case 1: {
+					SumFunction sum = (SumFunction)computeLib.resolve("sum");
+
+					if (sum)
+						res = sum(4.4, 10);
+					else
+						error = true;
+
+					break;
+				}
+				case 2: {
+					DifferenceFunction difference = (DifferenceFunction)computeLib.resolve("Compute::difference");
+
+					if (difference)
+						res = difference(4.4, 10);
+					else
+						error = true;
+
+					break;
+				}
+				case 3: {
+					ProductFunction product = (ProductFunction)computeLib.resolve("Compute::product");
+
+					if (product)
+						res = product(4.4, 10);
+					else
+						error = true;
+
+					break;
+				}
+				case 4: {
+					QuotientFunction quotient = (QuotientFunction)computeLib.resolve("Compute::quotient");
+
+					if (quotient)
+						res = quotient(4.4, 10);
+					else
+						error = true;
+
+					break;
+				}
+				case 5: {
+					AverageFunction average = (AverageFunction)computeLib.resolve("Compute::average");
+
+					if (average)
+						res = average(4.4, 10);
+					else
+						error = true;
+
+					break;
+				}
+				default: break;
+			}
 		} else
 			error = true;
 	} else
@@ -58,5 +149,8 @@ double loadFromLibrary()
 	if (error)
 		std::cerr << "\nError : " << computeLib.errorString().toStdString() << std::endl;
 
-	computeLib.unload();
+	if (computeLib.isLoaded())
+		computeLib.unload();
+
+	return res;
 }
